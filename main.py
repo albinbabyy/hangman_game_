1bafb09f1045c21bc2dd60a0d6cec91991202983
Albin Baby <albinbaby150@gmail.com>
2022-06-05 11:30:55 +0200

    one more feature added. if the letter guessed again. it will show you, the letter guessed already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
+from hangman_words import word_list
 
-word_list = ["aardvark" , "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 word_length = len(chosen_word)
@@ -17,20 +17,25 @@ while not False:
 
     guess = input("Enter your guess letter : \n").lower()
 
+    if guess in chosen_word:
+        print("already guessed")
     # to find the "postition" and need to insert letter in "dispaly".
     for position in range(word_length ):
         letter = chosen_word[position ]
         if letter == guess:
             display[position] = letter
 
+    print(f"{' ' .join(display)}")
+
     if guess not in chosen_word:
+
         lives -= 1
-        print(lives)
+        print(f"you have {lives} lives left")
         if lives == 0:
             end_of_game = True
             print("you lose")
             break
-    print(display)
+
     if "_" not in display:
         end_of_game = True
         print("you won")
