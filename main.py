4ad86379d83e90bd36c0952c920f83f4a76e9d46
Albin Baby <albinbaby150@gmail.com>
2022-06-05 11:05:12 +0200

    exit from the game when the blank complete.

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ print(chosen_word)
 word_length = len(chosen_word)
 
 display = []
-for space in range(word_length -1):
+for space in range(word_length ):
     display += "_"
 print(display)
 
-guess = input("Enter your guess letter : \n").lower()
 
-# to find the "postition" and need to insert letter in "dispaly".
-for position in range(word_length -1):
-    letter = chosen_word[position ]
-    if letter == guess:
-        display[position] = letter
-print(display)
+end_of_game = False
+while not False:
 
+    guess = input("Enter your guess letter : \n").lower()
+
+    # to find the "postition" and need to insert letter in "dispaly".
+    for position in range(word_length ):
+        letter = chosen_word[position ]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("you won")
+
+        break
 
 
 
